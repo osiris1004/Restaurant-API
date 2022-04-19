75ff8628363b1e04791a9c5fcfa530670571ced2
osiris1004 <kontchou05@gmail.com>
2022-04-19 12:13:44 -0400

    bigboss modle update

--- models/Bigboss_/BigBoss.js
+++ models/Bigboss_/BigBoss.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
+  const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
+const bigBossSchema = new Schema({
   username: {
     type: String,
     required: true,
@@ -17,7 +17,7 @@ const userSchema = new Schema({
   },
 });
 
-module.exports = mongoose.model("User", userSchema);
+module.exports = mongoose.model("User", bigBossSchema);
 
 
 
