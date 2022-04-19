afe82c70a873e678d2c1d8791dc10f0fe5ff451b
osiris1004 <kontchou05@gmail.com>
2022-04-19 12:21:33 -0400

    controler handlre request by admin

---
+++ controllers/Admin_/Admin.js
@@ -0,0 +1,63 @@
+const bcrypt = require("bcryptjs");
+
+const Admin = require("../../models/Bigboss_/Admin");
+
+
+
+exports.login_post = async (req, res) => {
+  
+  const { email, password } = req.body;
+  
+  //.findOne, return a row based on the passed property
+  const user = await Admin.findOne({ email });
+
+  if (!user) {
+    return res.send({"server" : "Invalid Credentials"})
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+
+  if (!isMatch) {
+    return res.send({"server" : "Invalid Credentials"})
+  }
+  console.log(req.session.email+"\n\n")
+  req.session.isAuth = true
+  req.session.email = user.email
+  req.session.path ='/admin'
+ 
+  console.log("am normally done")
+  
+
+
+  
+  //console.log( "done")
+  res.redirect("/admin/dashboard");
+ 
+};
+
+
+exports.dashboard_get = (req, res) => {
+  const email= req.session.email;
+  console.log(email)
+  return res.send({"server" : "Welcome "+email})
+};
+
+
+exports.logout_post = (req, res) => {
+  console.log(req.session)
+  if(!req.session.isAuth){
+    return res.send({"server" : "You can logout cuz you are not logout since your session was not identify"})
+  }
+  req.session.destroy((err) => {
+    if (err) throw err;
+    //res.redirect("/login");
+    
+    return res.send({"server" : "You logout"})
+
+  
+  });
+};
+
+
+
+
